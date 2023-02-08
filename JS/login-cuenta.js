c71a638cbec0b614c1f52848e25adf374487c0ba
Sham <mentulbenodorna@gmail.com>
2023-02-08 13:02:51 +0100

    script de login acabados

---
+++ JS/login-cuenta.js
@@ -0,0 +1,10 @@
+const saludar2 = document.getElementById('saludar2');
+    let nombre= localStorage.getItem('logeado');
+     if(nombre == null){
+        nombre = '';
+
+     }else{
+     console.log(nombre);
+    
+     saludar2.innerHTML = `¡Hola, Bienvenid@! ${nombre}`;
+    }
--- JS/login-home.js
+++ JS/login-home.js
@@ -1,6 +1,11 @@
 const saludarInicio = document.getElementById('saludar-home')
 let nombre= localStorage.getItem('logeado');
  
+if(nombre == null){
+    nombre = '';
+
+ }else{
  console.log(nombre);
 
  saludarInicio.innerHTML = `¡Hola, ${nombre}!`;
+}
