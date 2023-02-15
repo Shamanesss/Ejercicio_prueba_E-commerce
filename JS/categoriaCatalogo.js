67053103fceb8875723493489b0295de0bf90eed
YorlinPacha <yorlin1517@gmail.com>
2023-02-15 10:27:53 +0100

    Implementacion lista de categorias solo para HOMBRES

--- JS/categoriaCatalogo.js
+++ JS/categoriaCatalogo.js
@@ -1,48 +1,105 @@
- let checkZapatillaHombre = document.getElementById("checkZapatillaHombre");
- let checkBotasHombre = document.getElementById("checkBotasHombre");
- let checkSandaliasHombre = document.getElementById("checkSandaliasHombre");
+//Capturar caja que contiene los botones y cada id de los radios
+let selector = document.getElementById("cajaRadioHombre");
+let checkZapatillaHombre = document.getElementById("checkZapatillaHombre");
+let checkBotasHombre = document.getElementById("checkBotasHombre");
+let checkSandaliasHombre = document.getElementById("checkSandaliasHombre");
+let checkTodo = document.getElementById("checkTodoHombre");
 
- let hombreZapatilla = document.querySelector(".hombreZapatilla");
- let hombreBotas = document.querySelector(".hombreBotas");
- let hombreSandalias = document.querySelector(".hombreSandalias");
+//Capturar cada caja donde se muestra el contenido hombre
+let hombreZapatilla = document.querySelector(".hombreZapatilla");
+let hombreZapatillaCard1 = document.querySelector(".hombreZapatillaCard1");
+let hombreZapatillaCard2 = document.querySelector(".hombreZapatillaCard2");
+let hombreZapatillaCard3 = document.querySelector(".hombreZapatillaCard3");
+let hombreBotas = document.querySelector(".hombreBotas");
+let hombreBotasCard = document.querySelector(".hombreBotasCard");
+let hombreSandalias = document.querySelector(".hombreSandalias");
+let hombreSandaliasCard = document.querySelector(".hombreSandaliasCard");
 
+//checkZapatillaHombre.addEventListener("change", habilitar);
 
- checkZapatillaHombre.addEventListener("change", habilitar);
-
-
- //checkZapatillaHombre.checked=false;
- 
+//checkZapatillaHombre.checked=false;
 
 // ---------habilitar boton--------
 
 //checkZapatillaHombre.checked= false;
-console.log(checkZapatillaHombre.checked);
+// console.log(checkZapatillaHombre.checked);
 
-function habilitar(event) {
-        console.log(checkZapatillaHombre.checked)
-        let pulsado = checkZapatillaHombre.checked;
-        console.log(pulsado);
-        console.log(event);
+selector.addEventListener("change", function (event) {
+  // alert(event.target.id);
 
-        switch (pulsado) {
-          case true:
-              hombreZapatilla.style.backgroundColor = "red";
-              console.log(pulsado + "true");
-            break;
-      
-          case false:
-              hombreZapatilla.style.backgroundColor = "blue";
-              console.log(pulsado + "false");
-            break;
-        }
-  
- 
+  let identificador = event.target.id;
+  //alert(identificador);
+
+  //const mostrado=document.getElementById(identificador);
+
+  //alert(mostrado.index);
+
+  cambiarEstado(identificador);
+  console.log("captura id" + " " + identificador);
+});
+
+function cambiarEstado(identificador) {
+  console.log(identificador);
+  let pulsado = identificador;
+  console.log("captura en funcion" + "" + +pulsado);
+
+  switch (pulsado) {
+    case "checkTodoHombre":
+      //mostrar zapatillas
+      hombreZapatilla.style.display = "block";
+      hombreZapatillaCard1.style.display = "flex";
+      hombreZapatillaCard2.style.display = "flex";
+      hombreZapatillaCard3.style.display = "flex";
+      //mostrar botas
+      hombreBotas.style.display = "block";
+      hombreBotasCard.style.display = "flex";
+      //mostrar sandalias
+      hombreSandalias.style.display = "block";
+      hombreSandaliasCard.style.display = "flex";
+      break;
+
+    case "checkZapatillaHombre":
+      //mostrar
+      hombreZapatilla.style.display = "block";
+      hombreZapatillaCard1.style.display = "flex";
+      hombreZapatillaCard2.style.display = "flex";
+      hombreZapatillaCard3.style.display = "flex";
+      //ocultar
+      hombreBotas.style.display = "none";
+      hombreBotasCard.style.display = "none";
+      hombreSandalias.style.display = "none";
+      hombreSandaliasCard.style.display = "none";
+      console.log(pulsado + "true");
+      break;
+
+    case "checkBotasHombre":
+      //mostrar
+      hombreBotas.style.display = "block";
+      hombreBotasCard.style.display = "flex";
+      //ocultar
+      hombreZapatilla.style.display = "none";
+      hombreZapatillaCard1.style.display = "none";
+      hombreZapatillaCard2.style.display = "none";
+      hombreZapatillaCard3.style.display = "none";
+      hombreSandalias.style.display = "none";
+      hombreSandaliasCard.style.display = "none";
+
+      console.log(pulsado + "false");
+      break;
+    case "checkSandaliasHombre":
+       //mostrar
+       hombreSandalias.style.display = "block";
+      hombreSandaliasCard.style.display = "flex";
+      //ocultar
+      hombreZapatilla.style.display = "none";
+      hombreZapatillaCard1.style.display = "none";
+      hombreZapatillaCard2.style.display = "none";
+      hombreZapatillaCard3.style.display = "none";
+      hombreBotas.style.display = "none";
+      hombreBotasCard.style.display = "none";
+      break;
+  }
 }
-
-
-
-
-
 
 //  function estadoCheck(){
 //      console.log(checkZapatillaHombre.checked)
@@ -50,7 +107,7 @@ function habilitar(event) {
 //      if(estado.checked){
 //          console.log(checkZapatillaHombre)
 //          hombreZapatilla.style.backgroundColor = "red";
-        //  hombreZapatilla.style.display = "none";
-        //  hombreBotas.style.display = "none";
-        //  hombreSandalias.style.display = "none";
-    //} }
+//  hombreZapatilla.style.display = "none";
+//  hombreBotas.style.display = "none";
+//  hombreSandalias.style.display = "none";
+//} }
