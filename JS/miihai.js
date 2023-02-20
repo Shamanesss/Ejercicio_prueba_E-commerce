cba931d206c6358ca5d8db123b07cf0d6b7339a1
Jonathaneche <jonathan.eche@gmail.com>
2023-02-20 11:20:39 +0100

    actualizamos los archivos mihai.js con el respaldo de Gorka

--- JS/miihai.js
+++ JS/miihai.js
@@ -35,6 +35,7 @@ for (let i=0; i<cantidades.length; i++){
 };
 
 function mi (cantidad, precio, subtotal){
+
 let result = (cantidad * precio); 
 subtotal.innerHTML = "$" + result;
 }
@@ -59,17 +60,22 @@ localStorage.setItem("totalCheckout" , JSON.stringify(totalPagar));
 
 //totalPagar = suma;
 console.log(totalPagar);
+ 
+
 let vaciar = document.getElementById("vaciartodo")
 //vaciar.addEventListener("click",eliminarZapato)
+
 vaciar.addEventListener("click",()=>{
 
  eliminarZapatoMihai() })
 
 
 carrito.addEventListener("click",eliminarZapato);
+
  function eliminarZapatoMihai(){
     
  while(listaZapatos.firstChild){
+    
      listaZapatos.removeChild(listaZapatos.firstChild);
     }
    
@@ -77,6 +83,7 @@ carrito.addEventListener("click",eliminarZapato);
     return false;
   
  function vaciarLocalStorage(){
+
      //total(0);
      localStorage.clear();
     
@@ -85,45 +92,45 @@ carrito.addEventListener("click",eliminarZapato);
 
 
 
-//  function eliminarZapato(e){
+ function eliminarZapato(e){
 
     
-//     let zapato, zapatoId;
-//     //identificamos el enlace que tiene la clase borrar producto, en este caso la X
-//     if (e.target.classList.contains("carrito-producto-delete")){
-//        // console.log(e.target.parentElement.parentElement);
-//        //eliminamos la card
-//        e.target.parentElement.parentElement.remove();
-//         zapato = e.target.parentElement.parentElement;
-//         zapatoId = zapato.querySelector("button").getAttribute("data-id");
-//         //console.log(zapatoId);
+    let zapato, zapatoId;
+    //identificamos el enlace que tiene la clase borrar producto, en este caso la X
+    if (e.target.classList.contains("carrito-producto-delete")){
+       // console.log(e.target.parentElement.parentElement);
+       //eliminamos la card
+       e.target.parentElement.parentElement.remove();
+        zapato = e.target.parentElement.parentElement;
+        zapatoId = zapato.querySelector("button").getAttribute("data-id");
+        //console.log(zapatoId);
 
-//     }
+    }
 
-//     //paso 8  llamar funcion para eliminar del local storage al dar X
-//     eliminarProductoLocalStorage(zapatoId);
+    //paso 8  llamar funcion para eliminar del local storage al dar X
+    eliminarProductoLocalStorage(zapatoId);
 
-// }
+}
 
-// function eliminarProductoLocalStorage(zapato){
+function eliminarProductoLocalStorage(zapato){
  
-//     //console.log(zapato);
-//     let zapatosLS;
-//     // obtenemos el arreglo de zapatos
-//     zapatosLS = obtenerProductoLocalStorage();
+    //console.log(zapato);
+    let zapatosLS;
+    // obtenemos el arreglo de zapatos
+    zapatosLS = obtenerProductoLocalStorage();
 
-//     //Iteramos comparando el ID del zapato borrado con los del LS    
-//     zapatosLS.forEach(function(zapatoLS, index){
-//         // si el zapato id de la iteracion actual es igual al zapato que estamos eliminando fuera de la funcion entonces eliminamos ese zapato...
-//         //console.log(zapato.id);
-//         if(zapatoLS.id === zapato){
-//             zapatosLS.splice(index,1);
-//         }
-//     });
-//     //console.log(zapatosLS);
-//     //añadimos el arreglo actual a storage
-//     localStorage.setItem("zapatos",JSON.stringify(zapatosLS));
-// }
+    //Iteramos comparando el ID del zapato borrado con los del LS    
+    zapatosLS.forEach(function(zapatoLS, index){
+        // si el zapato id de la iteracion actual es igual al zapato que estamos eliminando fuera de la funcion entonces eliminamos ese zapato...
+        //console.log(zapato.id);
+        if(zapatoLS.id === zapato){
+            zapatosLS.splice(index,1);
+        }
+    });
+    //console.log(zapatosLS);
+    //añadimos el arreglo actual a storage
+    localStorage.setItem("zapatos",JSON.stringify(zapatosLS));
+}
 
 
 function eliminarZapato2(id){
